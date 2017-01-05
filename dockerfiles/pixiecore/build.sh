d0171d927813c1b29b0f9ccf0c85b0b173f53a71
pancho horrillo <pancho@pancho.name>
2017-01-05 19:37:22 +0100

    dockerfiles: Simplify build steps
    go build will honour GOBIN and leave the executable there.

--- dockerfiles/pixiecore/build.sh
+++ dockerfiles/pixiecore/build.sh
@@ -19,9 +19,7 @@ go get -v -d go.universe.tf/netboot/cmd/pixiecore
 cd /tmp/go/src/go.universe.tf/netboot
 glide install
 go test $(glide nv)
-cd cmd/pixiecore
-go build .
-cp ./pixiecore /pixiecore
+GOBIN=/ go install ./cmd/pixiecore
 cd /
 apk del --purge git go glide musl-dev
 rm -rf /tmp/go /tmp/stuff /var/cache/apk/*
